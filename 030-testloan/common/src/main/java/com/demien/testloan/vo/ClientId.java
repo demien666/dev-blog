4ea9f34d3a3f140ec26fe7aac2374e3971dc5ddc
dmitry.n.kovalsky <dmitry.n.kovalsky@gmail.com>
2017-07-07 22:30:49 +0200

    testloan - remove

--- 030-testloan/common/src/main/java/com/demien/testloan/vo/ClientId.java
+++
@@ -1,9 +0,0 @@
-package com.demien.testloan.vo;
-
-public class ClientId {
-    private final Long id;
-
-    public ClientId(Long id) {
-        this.id = id;
-    }
-}
--- 030-testloan/loan/src/main/java/com/demien/testloan/enums/LoanState.java
+++
@@ -1,5 +0,0 @@
-package com.demien.testloan.enums;
-
-public enum LoanState {
-    CREATED, APPROVED, EXTENDED, CLOSED
-}

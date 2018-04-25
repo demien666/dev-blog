b1b7cfc0c33e8f49dd3b2da1bfd2a7244829fc2b
Dmytro Kovalskyi <dmytro.kovalskyi@globallogic.com>
2018-04-25 11:20:04 +0200

    034 - streaming - streaming itself

---
+++ 034-spark-stream/src/main/scala/com/demien/sparktest/DataFrameFromFile.scala
@@ -0,0 +1,32 @@
+package com.demien
+
+// https://spark.apache.org/docs/latest/sql-programming-guide.html
+
+import org.apache.spark.sql.SparkSession
+
+object DataFrameFromFile extends App {
+
+  val spark = SparkSession
+    .builder()
+    .appName("Spark SQL basic example")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  // For implicit conversions like converting RDDs to DataFrames
+  import spark.implicits._
+
+
+  val df = spark.read.json("src/main/resources/people.json")
+  df.printSchema()
+  df.createOrReplaceTempView("people")
+  val sqlDF = spark.sql("SELECT * FROM people where email like '%net%' ")
+  sqlDF.show()
+
+  case class Person(name: String, email: String, city: String, mac: String, timestamp: String, creditcard: String)
+
+  val peopleDS = spark.read.json("src/main/resources/people.json").as[Person]
+  val filteredDS = peopleDS.filter(p => p.email != null && p.email.contains("net"))
+  filteredDS.show()
+
+
+}

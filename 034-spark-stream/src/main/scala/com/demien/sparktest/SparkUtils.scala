6ed8b3fd1fb5deaec4f8b7f3334ee3c929795d1c
demien <demien@debian>
2018-04-25 20:58:24 +0200

    034 - spark - changes

--- 034-spark-stream/src/main/scala/com/demien/sparktest/SparkUtils.scala
+++ 034-spark-stream/src/main/scala/com/demien/sparktest/SparkUtils.scala
@@ -1,24 +1,26 @@
 package com.demien.sparktest
 
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
 
 //import org.apache.spark.sql.SparkSession
 
 object SparkUtils {
 
-  /*
-  def getSpark = SparkSession
-    .builder()
-    .appName("Spark SQL basic example")
-    .master("local[2]")
-    .getOrCreate()
+  val DEF_APP_NAME = "MySparkApp"
+  val DEF_MASTER = "local[2]"
 
-*/
-
+  //The SparkContext object - connection to a Spark execution environment and created RDDs
   def getSparkContext(appName: String, master: String)  = new SparkContext( new SparkConf().setAppName(appName).setMaster(master) )
-  def getSparkContext(appName: String): SparkContext  = getSparkContext(appName, "local[2]")
-  def getSparkContext():SparkContext = getSparkContext("MySparkApp")
+  def getSparkContext = getSparkContext(DEF_APP_NAME, DEF_MASTER)
 
+  //The SparkSession - connection to dataframes and SQLs
+  def getSparkSession(appName: String, master: String) = SparkSession
+    .builder()
+    .appName(appName)
+    .master(master)
+    .getOrCreate()
+  def getSparkSession = getSparkSession(DEF_APP_NAME, DEF_MASTER)
 
 
 }
